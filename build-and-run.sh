20c1844517bb9569ede3aa1a373c782634eee7ee
Kier Davis <kierdavis@gmail.com>
2019-12-27 23:32:58 +0000

    Rename executable from cheese to pout

--- build-and-run.sh
+++ build-and-run.sh
@@ -3,4 +3,4 @@ set -o errexit -o nounset -o pipefail
 pkg=/tmp/pout-build
 nix-build -o $pkg -E 'with import <nixpkgs> {}; callPackage ./default.nix {}'
 export G_MESSAGES_DEBUG=all
-exec $pkg/bin/cheese "$@"
+exec $pkg/bin/pout "$@"
