3102fd9fb3fd83d3b7611869e047a652dfd14f2f
Kier Davis <kierdavis@gmail.com>
2019-12-28 01:40:54 +0000

    Adjust debug logging

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,5 +2,6 @@
 set -o errexit -o nounset -o pipefail
 pkg=/tmp/pout-build
 nix-build -o $pkg -E 'with import <nixpkgs> {}; callPackage ./default.nix {}'
-export G_MESSAGES_DEBUG=all
-exec $pkg/bin/pout "$@"
+#export G_MESSAGES_DEBUG=all
+#export GST_DEBUG=4  # INFO
+$pkg/bin/pout
