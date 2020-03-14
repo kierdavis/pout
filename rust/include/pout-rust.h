d3ce1e8456a5b90dfdecac984116364780fe5696
Kier Davis <kierdavis@gmail.com>
2020-03-14 23:18:49 +0000

    Explicitly include header that provides required types

--- rust/include/pout-rust.h
+++ rust/include/pout-rust.h
@@ -1,6 +1,8 @@
 #ifndef __POUT_RUST_H
 #define __POUT_RUST_H
 
+#include <glib.h>
+
 // Expect this API surface to develop rapidly...
 
 extern gint pout_compare_formats(gconstpointer a, gconstpointer b);
